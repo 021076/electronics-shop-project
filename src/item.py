520ac47fbd72d1b87785094e2105dfceab1b0608
021076 <ro_k_sana@mail.ru>
2023-07-11 21:59:27 +0500

    домашняя работа 13.2

--- src/item.py
+++ src/item.py
@@ -1,5 +1,11 @@
+import csv
+import os
+
+
 class Item:
     """ Класс для представления товара в магазине."""
+
+    # path_project: path = os.getenv("electronics-shop-project")
     pay_rate = 1.0
     all = []
 
@@ -9,7 +15,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
@@ -21,8 +27,36 @@ class Item:
         total_price = self.price * self.quantity
         return total_price
 
-    def apply_discount(self) -> None:
+    def apply_discount(self) -> float:
         """Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
         return self.price
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, newname):
+        self.__name = newname[:10]
+
+    @classmethod
+    def instantiate_from_csv(cls, csv_file='../src/items.csv'):
+        cls.all.clear()
+        with open(csv_file, encoding='windows-1251', ) as f:
+            reader = csv.DictReader(f)
+            for row_csv in reader:
+                item_list = list(row_csv.values())
+                name, price, quantity = item_list
+                price = float(price)
+                quantity = cls.string_to_number(quantity)
+                cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(from_str):
+        return int(float(from_str))
+
+
+
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -12,3 +12,18 @@ def test_calculate_total_price(some_item):
 def test_apply_discount(some_item):
     Item.pay_rate = 0.8
     assert some_item.apply_discount() == 4000.0
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
+def test_init():
+    item_10w = Item("Смартфон", 10000.50, 15)
+    assert item_10w.name == "Смартфон"
+    item_10w.name = "СуперСмартфон"
+    assert item_10w.name == "СуперСмарт"
