8ba90c44a47255702f870b10f053e6210fedf811
021076 <ro_k_sana@mail.ru>
2023-11-15 22:38:48 +0500

    4 курс. DZ homework-1

--- src/item.py
+++ src/item.py
@@ -1,30 +1,34 @@
+import csv
+import os
+
+
 class Item:
-    """
-    Класс для представления товара в магазине.
-    """
+    """ Класс для представления товара в магазине."""
+
+    # path_project: path = os.getenv("electronics-shop-project")
     pay_rate = 1.0
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
-
+        """Создание экземпляра класса item.
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.__name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
 
     def calculate_total_price(self) -> float:
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
-
+        """Рассчитывает общую стоимость конкретного товара в магазине.
         :return: Общая стоимость товара.
         """
-        pass
+        total_price = self.price * self.quantity
+        return total_price
 
-    def apply_discount(self) -> None:
+    def apply_discount(self) -> float:
+        """Применяет установленную скидку для конкретного товара.
         """
-        Применяет установленную скидку для конкретного товара.
-        """
-        pass
+        self.price = self.price * self.pay_rate
+        return self.price
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+@pytest.fixture
+def some_item():
+    return Item("Товар", 5000, 50)
+
+def test_calculate_total_price(some_item):
+    assert some_item.calculate_total_price() == 250000
+
+def test_apply_discount(some_item):
+    Item.pay_rate = 0.8
+    assert some_item.apply_discount() == 4000.0
+
