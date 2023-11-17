45697ab298e07f8d144e9288dbc1bd96cc183ecf
021076 <ro_k_sana@mail.ru>
2023-11-18 00:02:16 +0500

    4 курс. DZ homework-2. В main пришлось подправить путь к файлу из 'src/items.csv' в  '../src/items.csv', иначе файл не находился

--- src/item.py
+++ src/item.py
@@ -5,7 +5,7 @@ import os
 class Item:
     """ Класс для представления товара в магазине."""
 
-    # path_project: path = os.getenv("electronics-shop-project")
+    # path_project: os.path = os.getenv("electronics-shop-project")
     pay_rate = 1.0
     all = []
 
@@ -32,3 +32,30 @@ class Item:
         """
         self.price = self.price * self.pay_rate
         return self.price
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, newname):
+        self.__name = newname[:10]
+
+    @classmethod
+    def instantiate_from_csv(cls, csv_file):
+        cls.all.clear()
+        with open(csv_file, encoding='windows-1251', ) as f:
+            reader = csv.DictReader(f)
+            for row_csv in reader:
+                item_list = list(row_csv.values())
+                name, price, quantity = item_list
+                price = float(price)
+                quantity = cls.string_to_number(quantity)
+                cls(name, price, quantity)
+
+
+    @staticmethod
+    def string_to_number(from_str):
+        return int(float(from_str))
+
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -2,14 +2,30 @@
 import pytest
 from src.item import Item
 
+
 @pytest.fixture
 def some_item():
     return Item("Товар", 5000, 50)
 
+
 def test_calculate_total_price(some_item):
     assert some_item.calculate_total_price() == 250000
+
 
 def test_apply_discount(some_item):
     Item.pay_rate = 0.8
     assert some_item.apply_discount() == 4000.0
 
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('../src/items.csv')
+    assert len(Item.all) == 5
+    assert Item.all[0].name == 'Смартфон'
+    assert Item.all[1].price == 1000.0
+    assert Item.all[2].quantity == 5
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
