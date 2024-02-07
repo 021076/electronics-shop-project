5524ee86142bbd8e6fb5b9b7310c2eddee0959a2
021076 <ro_k_sana@mail.ru>
2024-02-07 14:26:10 +0500

    DZ. homework-4 - перенос фикстур в отдельный файл

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,12 @@
 """Набор фикстур для тестов"""
-
 import pytest
 from src.item import Item
 from src.phone import Phone
-from src.keyboard import Keyboard
 
 
 @pytest.fixture
 def some_item():
     return Item("Товар", 5000, 50)
-
-
-@pytest.fixture
-def some_keyboard():
-    return Keyboard("Keyboard XV-8", 2750, 31)
 
 
 @pytest.fixture
