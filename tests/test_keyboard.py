2931b09f4752465edc6882410cb44f06e11622cd
021076 <ro_k_sana@mail.ru>
2024-02-15 11:14:51 +0500

    DZ. homework-5. Исправление по рекомендации наставника: в тестах не нужно приводить к строке строковое значение assert str(some_keyboard...

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -9,15 +9,15 @@ def test__repr__(some_keyboard):
 
 def test__str__(some_keyboard):
     assert some_keyboard.__str__() == 'Keyboard XV-8'
-    assert str(some_keyboard.language) == "EN"
+    assert some_keyboard.language == "EN"
 
 
 def test_change_lang(some_keyboard):
-    assert str(some_keyboard.language) == "EN"
+    assert some_keyboard.language == "EN"
     some_keyboard.change_lang()
-    assert str(some_keyboard.language) == "RU"
+    assert some_keyboard.language == "RU"
     some_keyboard.change_lang()
-    assert str(some_keyboard.language) == "EN"
+    assert some_keyboard.language == "EN"
 
 
 def test_language(some_keyboard):
